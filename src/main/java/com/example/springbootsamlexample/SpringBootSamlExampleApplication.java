e0a0b4f6c7d1ead749ba782a3783c01ac761b666
piyushkumar13 <piyush.k9013@gmail.com>
2024-02-09 23:30:59 +0530

    initial commit

---
+++ src/main/java/com/example/springbootsamlexample/SpringBootSamlExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootsamlexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSamlExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootSamlExampleApplication.class, args);
+    }
+
+}
